0788fe72b9a83c0fe48a1aef4bd37cf939cce5d8
Pether Silva <petherjs@motorola.com>
2020-09-26 18:07:25 -0300

    handling user interactions methods

--- app/src/main/java/com/pethersilva/devapps20201_helloworld/MainActivity.kt
+++ app/src/main/java/com/pethersilva/devapps20201_helloworld/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.pethersilva.devapps20201_helloworld
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+
+		button.setOnClickListener {
+			Toast.makeText(this, "Dessa forma é no Kotlin",
+				Toast.LENGTH_SHORT).show()
+		}
+
+		val myButton = findViewById<Button>(R.id.buttonFindViewById)
+		myButton.setOnClickListener { Toast.makeText(this@MainActivity,
+			"Usando findViewById", Toast.LENGTH_SHORT).show() }
+	}
+
+	fun handleOnClickEvent(view: View) {
+		Toast.makeText(this, "onClickEvent",
+			Toast.LENGTH_SHORT).show()
+	}
 }
