64872ff8f3a2ee12513349aa044b25daaecd5eaf
Pether Silva <petherjs@motorola.com>
2020-09-29 11:05:19 -0300

    adding start activity for result sample

--- app/src/main/java/com/pethersilva/devapps20201_helloworld/MainActivity.kt
+++ app/src/main/java/com/pethersilva/devapps20201_helloworld/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.pethersilva.devapps20201_helloworld
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
@@ -8,21 +9,40 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 	companion object {
-		const val MAIN_ACTIVITY_NAME_EXTRA_ID = "name"
+		const val MAIN_ACTIVITY_VALUE01_EXTRA_ID = "value01"
+		const val MAIN_ACTIVITY_VALUE02_EXTRA_ID = "value02"
+		const val MAIN_ACTIVITY_SUM_REQUEST_CODE = 1
+		const val MAIN_ACTIVITY_SUM_RESULT_EXTRA_ID = "sum_result_extra"
 	}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-		button.setOnClickListener {
-			val name = editTextTextPersonName.text.toString()
-			if (isNameValid(name)) {
+
+		buttonSum.setOnClickListener {
+			val value01 = editTextValue01.text.toString()
+			val value02 = editTextValue02.text.toString()
+			if (isValid(value01) && isValid(value02)) {
 				val secondActivity = Intent(this, SecondActivity::class.java)
-				secondActivity.putExtra(MAIN_ACTIVITY_NAME_EXTRA_ID, name)
-				startActivity(secondActivity)
+				secondActivity.putExtra(MAIN_ACTIVITY_VALUE01_EXTRA_ID, value01)
+				secondActivity.putExtra(MAIN_ACTIVITY_VALUE02_EXTRA_ID, value02)
+				startActivityForResult(secondActivity, MAIN_ACTIVITY_SUM_REQUEST_CODE)
 			} else {
-				Toast.makeText(this, "Please fill your name", Toast.LENGTH_SHORT).show()
+				Toast.makeText(this, "Please inform values", Toast.LENGTH_SHORT).show()
 			}
 		}
 	}
-	private fun isNameValid(name: String): Boolean = !name.isNullOrEmpty()
+
+	override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+		super.onActivityResult(requestCode, resultCode, data)
+		if (resultCode == Activity.RESULT_OK) {
+			if (MAIN_ACTIVITY_SUM_REQUEST_CODE == requestCode) {
+				val result = data?.getStringExtra(MAIN_ACTIVITY_SUM_RESULT_EXTRA_ID)
+				Toast.makeText(this, "Result: $result", Toast.LENGTH_SHORT).show()
+			}
+		}
+	}
+
+	private fun isValid(name: String): Boolean = !name.isNullOrEmpty()
 }
+
--- app/src/main/java/com/pethersilva/devapps20201_helloworld/SecondActivity.kt
+++ app/src/main/java/com/pethersilva/devapps20201_helloworld/SecondActivity.kt
@@ -1,15 +1,32 @@
 package com.pethersilva.devapps20201_helloworld
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_second.*
 
+
 class SecondActivity : AppCompatActivity() {
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_second)
-		val name = intent.getStringExtra(MainActivity.MAIN_ACTIVITY_NAME_EXTRA_ID)
-		val result = getString(R.string.second_activity_message, name)
-		textView.text = result
+		val value01 = intent.getStringExtra(MainActivity.MAIN_ACTIVITY_VALUE01_EXTRA_ID)
+		val value02 = intent.getStringExtra(MainActivity.MAIN_ACTIVITY_VALUE02_EXTRA_ID)
+
+		if (value01 != null && value02 != null) {
+			textView.text = sumValues(value01.toDouble(), value02.toDouble()).toString()
+		}
+
+		buttonResult.setOnClickListener {
+			val returnIntent = Intent()
+			returnIntent.putExtra(MainActivity.MAIN_ACTIVITY_SUM_RESULT_EXTRA_ID, textView.text)
+			setResult(Activity.RESULT_OK, returnIntent)
+			finish()
+		}
 	}
+
+	private fun sumValues(value01: Double, value02: Double) = value01 + value02
 }
+
