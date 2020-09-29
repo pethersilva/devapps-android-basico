34d119a5fbd7d39a94fab2a8816e037adbcf4326
Pether Silva <petherjs@motorola.com>
2020-09-29 16:59:05 -0300

    adding logs to verity activity lifecycle

--- app/src/main/java/com/pethersilva/devapps20201_helloworld/MainActivity.kt
+++ app/src/main/java/com/pethersilva/devapps20201_helloworld/MainActivity.kt
@@ -2,6 +2,7 @@ package com.pethersilva.devapps20201_helloworld
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -9,10 +10,18 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 	companion object {
 		const val MAIN_ACTIVITY_NAME_EXTRA_ID = "name"
+		const val TAG = "lifecycle"
+		const val SAVED_INSTANCE_EXTRA_ID = "saved_instance_extra_id"
 	}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+		if (savedInstanceState != null) {
+			val saved = savedInstanceState.getFloat(SAVED_INSTANCE_EXTRA_ID)
+			Log.d(TAG, "onCreate not null: $saved")
+		}
+
 		button.setOnClickListener {
 			val name = editTextTextPersonName.text.toString()
 			if (isNameValid(name)) {
@@ -23,6 +32,50 @@ class MainActivity : AppCompatActivity() {
 				Toast.makeText(this, "Please fill your name", Toast.LENGTH_SHORT).show()
 			}
 		}
+		Log.d(TAG, "onCreate()")
+	}
+
+	override fun onStart() {
+		super.onStart()
+		Log.d(TAG, "onStart()")
+	}
+
+	override fun onResume() {
+		super.onResume()
+		Log.d(TAG, "onResume()")
+	}
+
+	override fun onRestart() {
+		super.onRestart()
+		Log.d(TAG, "onRestart()")
+	}
+
+	override fun onPause() {
+		super.onPause()
+		Log.d(TAG, "onPause()")
+	}
+
+	override fun onStop() {
+		super.onStop()
+		Log.d(TAG, "onStop()")
+	}
+
+	override fun onDestroy() {
+		super.onDestroy()
+		Log.d(TAG, "onDestroy()")
+	}
+
+	override fun onSaveInstanceState(outState: Bundle) {
+		super.onSaveInstanceState(outState)
+		outState.putFloat(SAVED_INSTANCE_EXTRA_ID, 10.1f)
+		Log.d(TAG, "onSaveInstanceState()")
+	}
+
+	override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+		super.onRestoreInstanceState(savedInstanceState)
+		val saved = savedInstanceState.getFloat(SAVED_INSTANCE_EXTRA_ID)
+		Log.d(TAG, "onRestoreInstanceState(): $saved")
+
 	}
 	private fun isNameValid(name: String): Boolean = !name.isNullOrEmpty()
 }
