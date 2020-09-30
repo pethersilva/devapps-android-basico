ec2f0ed8baf8fcab1746ed0c346722fdbab8a2c2
Pether Silva <petherjs@motorola.com>
2020-09-30 15:51:13 -0300

    adding an option to insert new states

--- app/src/main/java/com/pethersilva/devapps20201_helloworld/MainActivity.kt
+++ app/src/main/java/com/pethersilva/devapps20201_helloworld/MainActivity.kt
@@ -10,22 +10,36 @@ import kotlinx.android.synthetic.main.activity_main_listview.*
 
 class MainActivity : AppCompatActivity() {
 
-	private val listEstados = listOf(
+	private val listEstados = mutableListOf(
 		Estado("Paraíba", 0),
 		Estado("Pernambuco", 1),
 		Estado("Rio Grande do Norte", 2)
 	)
 
+	private val mEstadoAdapter by lazy { EstadoAdapter(this, listEstados) }
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main_listview)
-		val adapter = EstadoAdapter(this, listEstados)
+		setupListview()
 
+		buttonInserir.setOnClickListener {
+			val name = editTextPersonName.text.toString()
+			if (isNameValid(name)) {
+				listEstados.add(Estado(name, (0..2).random()))
+				mEstadoAdapter.notifyDataSetChanged()
+			}
+		}
+	}
+
+	private fun isNameValid(name: String): Boolean = !name.isNullOrEmpty()
+
+	private fun setupListview() {
 		listView.setOnItemClickListener { parent, view
 										  , position, id ->
-			val(nome, bandeira) = listEstados[position]
+			val (nome, bandeira) = listEstados[position]
 			Toast.makeText(this, "click: $$nome $bandeira", Toast.LENGTH_SHORT).show()
 		}
-		listView.adapter = adapter
+		listView.adapter = mEstadoAdapter
 	}
 }
